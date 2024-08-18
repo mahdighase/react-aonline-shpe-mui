4a33143366996d2ad5a7e8421001a23ea18c588f
bishor <147510389+mahdighase@users.noreply.github.com>
2024-08-18 10:29:53 -0700

    tasih

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,37 @@
-import { Container, styled } from '@mui/material'
-import { color, height } from '@mui/system'
-import React from 'react'
-import styled from 'styled-components'
-export default function Navbar() {
-    // const Left =styled.div
-    // const Center =styled.div
-    // const Right=styled.div
-    const Wrapper=styled.div``
-    
+import { Container } from '@mui/material';
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Navbar() {  
+  const Wrapper = styled.div`
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  `;
+
+  const Left = styled.div`
+    flex: 1;
+  `;
+
+  const Center = styled.div`
+    flex: 1;
+    text-align: center;
+  `;
+
+  const Right = styled.div`
+    flex: 1;
+    text-align: right;
+  `;
+
   return (
-    <Container  sx={{height:"60px",padding:'10px 20px', backgroundColor:'black',}}>
-     <Wrapper sx={{color:'white'}}>
-       <Left>left</Left> 
-       <Center>center</Center>
-       <Right>right</Right>
-     </Wrapper>
+    <Container sx={{ height: '60px', padding: '10px 20px', backgroundColor: 'black' }}>
+      <Wrapper>
+        <Left>left</Left> 
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
     </Container>
-  )
+  );
 }
